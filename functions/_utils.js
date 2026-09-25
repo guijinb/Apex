d3fc06254e1bc256ed84c81937741c92638f92c6
guijinb <ezuoju6@gmail.com>
2026-09-25 22:45:50 +0800

    fix: add charset=utf-8 to JSON response to fix Chinese encoding

--- functions/_utils.js
+++ functions/_utils.js
@@ -51,7 +51,7 @@ export function jsonResponse(data, status = 200) {
   return new Response(JSON.stringify(data), {
     status,
     headers: {
-      'Content-Type': 'application/json',
+      'Content-Type': 'application/json; charset=utf-8',
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'POST, OPTIONS',
       'Access-Control-Allow-Headers': 'Content-Type',
